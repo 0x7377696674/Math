f229eca75b04d298b7f7d62acaf7f72a5097fc68
0xSwifty <notfolklore1989@proton.me>
2026-02-12 20:01:59 -0300

    [UPDATE] add function to get unit vector

--- src/linear_algebra/vector.rs
+++ src/linear_algebra/vector.rs
@@ -4,13 +4,13 @@ use std::fmt;
 
 #[derive(Debug)]
 pub struct Vector {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: f64,
+    y: f64,
+    z: f64,
 }
 
 impl Vector {
-    pub fn new(x: i32, y: i32, z: i32) -> Self {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self {
             x,
             y,
@@ -18,13 +18,35 @@ impl Vector {
         }
     }
 
-    pub fn scale(&mut self, scalar: i32) {
+    pub fn info(&self, other: Self) {
+        let dot = self.dot(&other);
+
+        if dot > 0.0 {
+            println!("angle is less than 90");
+        } else if dot < 0.0 {
+            println!("angle is more than 90");
+        } else {
+            println!("angle is 90");
+        }
+    }
+
+    pub fn unit(&self) -> Self {
+        let dot = self.dot(self);
+
+        Self {
+            x: self.x / dot.sqrt(),
+            y: self.y / dot.sqrt(),
+            z: self.z / dot.sqrt(),
+        }
+    }
+
+    pub fn scale(&mut self, scalar: f64) {
         self.x *= scalar;
         self.y *= scalar;
         self.z *= scalar;
     }
 
-    pub fn dot(&self, other: &Vector) -> i32 {
+    pub fn dot(&self, other: &Vector) -> f64 {
         let x = self.x * other.x;
         let y = self.y * other.y;
         let z = self.z * other.z;
@@ -38,7 +60,7 @@ impl Vector {
         format!("||v|| = \u{221A}{}", dot)
     }
 
-    pub fn linear_combination(c: i32, mut x: Vector, d: i32, mut y: Vector, e: i32, mut z: Vector) -> Self {
+    pub fn linear_combination(c: f64, mut x: Vector, d: f64, mut y: Vector, e: f64, mut z: Vector) -> Self {
         x.scale(c);
         y.scale(d);
         z.scale(e);
