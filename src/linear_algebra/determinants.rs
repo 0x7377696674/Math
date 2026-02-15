a1589ffb435e758e44660251fc3b4dd648fa153b
0xSwifty <notfolklore1989@proton.me>
2026-02-14 22:51:13 -0300

    [TEXT] field axioms

---
+++ src/linear_algebra/determinants.rs
@@ -0,0 +1,23 @@
+// Determinants!
+
+// Number Fields
+//
+// By number field, we mean any set K of objects, which, after using any of the four operations
+// (addition, subtraction, division, multiplication) give elements of K.
+//
+// These operations have the following properties(field axioms).
+//
+// Addition
+// -Commutative -> a + b = b + a;
+// -Associative -> (a + b) + c = a + (b + c);
+// -Negative -> there's a number for every a in K that a + y = 0;
+// -Identity -> There is a number 0 (zero) in K such that 0 + a = a for every a in K;
+//
+// Multiplication
+// Commutative -> a * b = b * a;
+// Associative -> (a * b) * c = a * (b * c);
+// Identity -> for every a != 0 in K there's a number that a * y = 1;
+//
+// Multiplication is distributive over Addition
+// a(b + c) = ab + ac;
+
