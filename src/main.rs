12e553f7e3473c3c1cc1343966b1e1ad782887dc
0xSwifty <notfolklore1989@proton.me>
2026-02-23 22:15:41 -0300

    [UPDATE] add helper function on main to convert vector from i32 to f32

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@
 use math::prelude::*;
 
 fn main() {
-    let data = vec![2.0, 1.0, 3.0, 4.0, 3.0, 8.0, 1.0, 2.0, 5.0];
+    let data = vec![2, 1, 3, 4, 3, 8, 1, 2, 5];
     let row = 3;
     let column = 3;
-    let mut A = Matrix::from(data, row, column);
+    let mut A = Matrix::from(from_i32(data), row, column);
 
     A.print();
-    A.upper_triangular();
-    A.lower_triangular();
+    A.inverse();
+}
+
+fn from_i32(vector: Vec<i32>) -> Vec<f32> {
+    vector.iter().map(|&x| x as f32).collect()
 }
